b5402063485f5baea9243dc424fc15e1e56561a9
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-02 16:30:52 +0100

    little changes

--- src/components/announcement-bar/AnnouncementBar.jsx
+++ src/components/announcement-bar/AnnouncementBar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './announcementBarStyles.css'
 
 function AnnouncementBar() {
-  return (
-    <div className="announcement-bar-wrapper">
-        <div className="row">
-            <div className="col-md-12">
-            <p className='text-uppercase'>FREE SHIPPING + EASY RETURNS & EXCHANGES</p>
+    return (
+        <div className="announcement-bar-wrapper">
+            <div className="row">
+                <div className="col-md-12">
+                    <p className='text-uppercase'><a href="">FREE SHIPPING + EASY RETURNS & EXCHANGES</a></p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AnnouncementBar
