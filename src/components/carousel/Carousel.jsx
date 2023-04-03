7f0b5816ea38e9c79fb2bab27194c4ae0f79299a
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-03 16:12:49 +0100

    changes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -6,17 +6,13 @@ export default function Carousel() {
     return (
         <div className="carousel-wrapper">
             <div id="carouselExampleCaptions" class="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src={mainCarouselImg} class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            <div className="btn text-dark text-uppercase mx-2">shop new releases</div>
+                            <div className="btn text-dark text-uppercase mx-2">925 silver </div>
+
                         </div>
                     </div>
                 </div>
