b0495d1da97df53b0e8448d26c3beab04120770a
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 00:36:23 -0700

    changes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import './CarouselStyles.css'
 import mainCarouselImg from '../../imgs/main-carousel-img.jpg'
 import mobileCarousel from '../../imgs/mobile-carousel.png'
@@ -7,13 +7,13 @@ export default function Carousel() {
 
     const [isImageLoaded, setIsImageLoaded] = useState(false);
     return (
+
         <div className="carousel-wrapper">
             <div id="carouselExampleCaptions" class="carousel slide">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                   <div className="mob"> <img src={mobileCarousel} className={` d-none w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} /></div>
-                    <div className="wide"><img src={mainCarouselImg} className={`d-lg-block  w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
-</div>
+                        <div className="wide"><img src={mainCarouselImg} className={` d-none d-lg-block  w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
+                        </div>
                         <div class="carousel-caption d-md-block">
                             <div className="btn text-dark text-uppercase mx-2">shop new releases</div>
                             <div className="btn text-dark text-uppercase mx-2">925 silver </div>
