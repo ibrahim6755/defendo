ca96d6cd05eca674470eb14d5e25dbd9bf164d76
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-03 19:13:59 -0700

    changes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './CarouselStyles.css'
 import mainCarouselImg from '../../imgs/main-carousel-img.jpg'
+import mobileCarousel from '../../imgs/mobile-carousel.png'
 
 export default function Carousel() {
+
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
     return (
         <div className="carousel-wrapper">
             <div id="carouselExampleCaptions" class="carousel slide">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src={mainCarouselImg} class="d-block w-100" alt="..." />
-                        <div class="carousel-caption d-none d-md-block">
+                    <img src={mobileCarousel} className={`mob d-none w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
+                        
+                    <img src={mainCarouselImg} className={`d-none wide w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
+
+                        <div class="carousel-caption d-md-block">
                             <div className="btn text-dark text-uppercase mx-2">shop new releases</div>
                             <div className="btn text-dark text-uppercase mx-2">925 silver </div>
 
