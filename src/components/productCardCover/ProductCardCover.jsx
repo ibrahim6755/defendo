390bc0be2b4761b4967ed8fc90735324cb8fcbdc
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 21:37:58 -0700

    changes

--- src/components/productCardCover/ProductCardCover.jsx
+++ src/components/productCardCover/ProductCardCover.jsx
@@ -9,8 +9,8 @@ function ProductCardCover() {
             <div className="card mb-3 border-0">
                 <div className="cover-pic"><img src={chainsCoverPic} alt="" /></div>
                 <div className="card-body">
-                <h5 className='card-text text-light text-uppercase fs-6'>made for 24/7 wear</h5>
-                    <h3 className="card-title text-light text-uppercase fs-6">chains</h3>
+                <h5 className='card-text text-light text-uppercase fs-16'>made for 24/7 wear</h5>
+                    <h3 className="card-title text-light text-uppercase fs-16">chains</h3>
                     <a href="#" className="btn btn-light text-uppercase text-dark ">View products</a>
                 </div>
             </div>
