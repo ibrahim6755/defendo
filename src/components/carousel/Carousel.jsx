5b30011be5f38e0c84caa856ebcf4f4c5ff90f90
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 18:34:47 -0700

    changes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from 'react'
 import './CarouselStyles.css'
 import mainCarouselImg from '../../imgs/main-carousel-img.jpg'
-import mobileCarousel from '../../imgs/mobile-carousel.png'
+import mainCarousel from '../../imgs/mainCarousel.jpeg'
 
 export default function Carousel() {
 
@@ -12,11 +12,13 @@ export default function Carousel() {
             <div id="carouselExampleCaptions" class="carousel slide">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <div className="wide"><img src={mainCarouselImg} className={` d-none d-lg-block  w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
+                        <div className="wide">
+                            <img src={mainCarousel} className={` d-none d-lg-block  w-100 fade-in ${isImageLoaded ? 'loaded' : ''}`} alt="..." onLoad={() => setIsImageLoaded(true)} />
+                            <div className="overlay"></div>
                         </div>
                         <div class="carousel-caption d-md-block">
-                            <div className="btn text-dark text-uppercase mx-2">shop new releases</div>
-                            <div className="btn text-dark text-uppercase mx-2">925 silver </div>
+                            <div className="btn text-light text-uppercase mx-2">shop new releases</div>
+                            <div className="btn text-light text-uppercase mx-2">925 silver </div>
 
                         </div>
                     </div>
