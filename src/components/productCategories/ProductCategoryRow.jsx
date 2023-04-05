f68dda25dee010fe7637f8040119e31b0c805701
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 20:41:24 -0700

    changes

--- src/components/productCategories/ProductCategoryRow.jsx
+++ src/components/productCategories/ProductCategoryRow.jsx
@@ -7,13 +7,13 @@ function ProductCategoryRow() {
   return (
     <div className="product-category-row-wrapper p-3">
        <div className="row p-2">
-        <div className="col-md-4 p-2">
+        <div className="col-md-4 col-sm 4 p-2">
         <ProductCardCover/>
         </div>
-        <div className="col-md-4 p-2">
+        <div className="col-md-4 col-sm 4 p-2">
         <ProductCardCover/>
         </div>
-        <div className="col-md-4 p-2">
+        <div className="col-md-4 col-sm 4 p-2">
         <ProductCardCover/>
         </div>
        </div>
