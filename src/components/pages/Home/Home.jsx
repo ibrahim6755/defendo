dc0310751feb9e9f5c62abdef572c15cb7ddbc57
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 18:01:27 -0700

    dropped section

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AnnouncementBar from '../../announcement-bar/AnnouncementBar'
 import NavBar from '../../NavBar/NavBar'
 import Carousel from '../../carousel/Carousel'
+import JustDropped from '../../justDropped/JustDropped'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <AnnouncementBar />
       <NavBar />
       <Carousel/>
+      <JustDropped/>
     </>
   )
 }
