ede479dcdc5354ce282fc9ca0a3bb12f05632659
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-04 18:10:28 -0700

    changes

--- src/components/justDropped/JustDropped.jsx
+++ src/components/justDropped/JustDropped.jsx
@@ -8,21 +8,28 @@ function JustDropped() {
        <div className="title">
           <h3 className='text-uppercase text-center fs-6'><i class="bi bi-arrow-down-short"></i> just dropped <i class="bi bi-arrow-down-short"></i></h3>
         </div>
-      <div className="container p-5">
+      <div className="container mt-5">
         <div className="view-product-wrapper">
+         <div className="row">
+          <div className="col-md-6">
           <div className="model-img">
             <img src={modelImg} alt=" Model Image" />
           </div>
+          
+          </div>
+          <div className="col-md-6">
           <div className="product mx-5">
             <div class="card border-0">
               <img src={droppedProduct} class="card-img-top" alt="Product Image" />
-              <div class="card-body w-100">
+              <div class="card-body">
                 <h5 class="card-title text-uppercase text-center">44 CALIBER RING</h5>
                 <p class="card-text text-center mb-4">1,500.00 PKR</p>
-               <div className="button"> <a href="#" class="btn btn-dark text-uppercase text-center card-title text-light px-3 py-2">view this product</a></div>
+               <div className="button"> <a href="#" class="btn btn-dark text-uppercase text-center card-title text-light p-4 py-2">view this product</a></div>
               </div>
             </div>
           </div>
+          </div>
+         </div>
         </div>
       </div>
     </div>
