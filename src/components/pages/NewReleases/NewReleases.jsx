118e440058ecce30f4d76dafb151c8270c4eab9d
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-06 08:35:49 -0700

    little changes

--- src/components/pages/NewReleases/NewReleases.jsx
+++ src/components/pages/NewReleases/NewReleases.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './NewReleasesStyles.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function NewReleases() {
     return (
@@ -32,19 +32,15 @@ function NewReleases() {
                             </div>
                         </div>
                         <div className="filter p-3 ">
-                            <button className="btn filter-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-                                <span>filter</span>
-                            </button>
-                            <div className="offcanvas offcanvas-end" data-bs-backdrop="static" tabIndex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-                                <div className="offcanvas-header pb-1">
-                                <h2 class="offcanvas-title" id="staticBackdropLabel">filters</h2>
-                                    <button type="button" className="btn-close fs-6" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <button class="btn text-uppercase" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"> <span>filters</span> </button>
+
+                            <div class="offcanvas offcanvas-end" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
-                                <hr />
-                                <div className="offcanvas-body">
-                                    <div className=' body-text text-center'>
-                                        your cart is empty
-                                    </div>
+                                <div class="offcanvas-body">
+                                    <p>Try scrolling the rest of the page to see this option in action.</p>
                                 </div>
                             </div>
                         </div>
