e30f68c033edc4bff6310b1b5ee2daa3be76cad0
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-05 17:23:41 -0700

    changes

--- src/components/ourInstaReviews/OurInstaReviews.jsx
+++ src/components/ourInstaReviews/OurInstaReviews.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import './OurInstaReviewsStyles.css'
+import review1 from '../../imgs/review1.png'
+import review2 from '../../imgs/review2.png'
+import review3 from '../../imgs/review3.png'
+import review4 from '../../imgs/review4.png'
+import review5 from '../../imgs/review5.png'
+import review6 from '../../imgs/review6.png'
+import review7 from '../../imgs/review7.png'
+import review8 from '../../imgs/review8.png'
+import review9 from '../../imgs/review9.png'
+import review10 from '../../imgs/review10.png'
 
 function OurInstaReviews() {
     return (
@@ -7,28 +17,32 @@ function OurInstaReviews() {
             <div className="row">
                 <h5 className='text-center '>CATCH UP WITH US ON INSTAGRAM <span>⚡️</span></h5>
                 <div className="col-md-4">
-                    <div className="reviews-lane">
-                        <div className="reviews-lane-1">
-                            <img className='w-25' src="" alt="" />
-                            <img className='w-25' src="" alt="" />
-
-                        </div><div className="reviews-lane-2">
-
+                    <div className="row row-imgs py-4">
+                        <div className="col-md-6 p-0">
+                            <a href=""><img className='w-100 my-2 px-2' src={review9} alt="" /></a>
+                            <a href=""><img className='w-100 my-2 px-2' src={review8} alt="" /></a>
+                        </div>
+                        <div className="col-md-6 p-0">
+                            <a href=""><img className='w-100 my-2 px-2' src={review3} alt="" /></a>
+                            <a href=""><img className='w-100 my-2 px-2' src={review4} alt="" /></a>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4">
-
+                <div className="col-md-4 p-2">
+                    <a href=""><img className='w-100' src={review5} alt="" /></a>
                 </div>
                 <div className="col-md-4">
-                    <div className="reviews-lane">
-                        <div className="reviews-lane-1">
-
-                        </div><div className="reviews-lane-2">
-
+                <div className="row row-imgs">
+                        <div className="col-md-6 p-0">
+                            <a href=""><img className='w-100 px-2 my-2 px-2' src={review4} alt="" /></a>
+                            <a href=""><img className='w-100 px-2 my-2 px-2' src={review3} alt="" /></a>
+                        </div>
+                        <div className="col-md-6 p-0">
+                            <a href=""><img className='w-100 my-2 px-2' src={review8} alt="" /></a>
+                            <a href=""><img className='w-100 my-2 px-2' src={review9} alt="" /></a>
                         </div>
                     </div>
-
+                   
                 </div>
             </div>
         </div>
