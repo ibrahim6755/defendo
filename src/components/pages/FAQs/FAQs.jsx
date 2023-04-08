4f9861d9657604646413d9cc823ce690493e6088
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-07 22:30:23 -0700

    changes

--- src/components/pages/FAQs/FAQs.jsx
+++ src/components/pages/FAQs/FAQs.jsx
@@ -35,11 +35,12 @@ function FAQs() {
                     <div className="accordion-item my-2">
                         <h2 className="accordion-header">
                             <button className="accordion-button collapsed text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                How do I know what size to order for ur accessories?
+                                Do you offer international shipping?
                             </button>
                         </h2>
                         <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">We provide detailed sizing information on each product page to help uh determine the best size of ur needs. If you have any questions or need additional assistance,our customer service team is always happy to help.</div>
+                            <div className="accordion-body"> No, we are currently dealing in Pakistan only.
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -52,11 +53,11 @@ function FAQs() {
                     <h5 className='fw-bold text-uppercase'>MATERIALS + PRODUCTS</h5>
                     <div className="accordion-item my-2 ">
                         <h2 className="accordion-header ">
-                            <button className="accordion-button collapsed text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            <button className="accordion-button collapsed text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
                                 Are your products sustainable?
                             </button>
                         </h2>
-                        <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div id="flush-collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                             <div className="accordion-body">Yes, we are committed to sustainability n ethical business practices.
 
                             </div>
@@ -64,22 +65,12 @@ function FAQs() {
                     </div>
                     <div className="accordion-item my-2">
                         <h2 className="accordion-header">
-                            <button className="accordion-button collapsed  text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                What sets your accessories apart from others on the market?
+                            <button className="accordion-button collapsed  text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                                How do I know what size to order for ur accessories?
                             </button>
                         </h2>
-                        <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Our accessories are designed with a focus on quality,style n versatility. N these are also designed to be versatile and adaptable to a range of styles n certain occasions.</div>
-                        </div>
-                    </div>
-                    <div className="accordion-item my-2">
-                        <h2 className="accordion-header">
-                            <button className="accordion-button collapsed text-light bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                Are ur products sustainable?
-                            </button>
-                        </h2>
-                        <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Yes, we are committed to sustainability n ethical business practices.</div>
+                        <div id="flush-collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div className="accordion-body">We provide detailed sizing information on each product page to help uh determine the best size of ur needs. If you have any questions or need additional assistance,our customer service team is always happy to help.</div>
                         </div>
                     </div>
                 </div>
