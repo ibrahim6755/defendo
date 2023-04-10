f109ff0659eb2058fccf039fde7c17838b25762c
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-10 07:44:18 -0700

    changes

--- src/components/pages/Product description/ProductDescription.jsx
+++ src/components/pages/Product description/ProductDescription.jsx
@@ -17,9 +17,38 @@ function ProductDescription() {
                         <a href="#golden"><img className=' d-block' src={productDesc2} alt="" /></a>
                     </div>
                 </div>
-                <div className="col-md-4 align-items-start mid-col">
+                <div className="col-md-4 align-items-start mid-col d-none d-md-block">
                     <Link to=""><img id='silver' className='my-4 d-block' src={productDesc1} alt="" /></Link>
                     <Link to=""><img id='golden' className='my-4 d-block' src={productDesc2} alt="" /></Link>
+
+                </div>
+                <div className="mob-col col-md-4 ">
+
+                    <div className="carousel">
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src={productDesc1} class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={productDesc2} class="d-block w-100" alt="..." />
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-md-5 right-col align-items-start">
                     <h5 className="card-title text-start">44 CALIBER RING</h5>
