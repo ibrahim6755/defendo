a017f07c5685604e67b54538757a81fd5d1feb47
ibrahim6755 <ibrahimghani020@gmail.com>
2023-04-10 03:56:38 -0700

    changes

--- src/components/pages/Product description/ProductDescription.jsx
+++ src/components/pages/Product description/ProductDescription.jsx
@@ -1,30 +1,67 @@
 import React from 'react'
 import './ProductDescriptionStyles.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import productDesc1 from '../../../imgs/product-desc-1.jpeg'
 import productDesc2 from '../../../imgs/product-desc-2.jpeg'
 
+import reviewStar from '../../../imgs/reviewStar.png'
+
 function ProductDescription() {
-  return (
-    <div className="product-description-wrapper">
-        <div className="row p-2">
-            <div className="col-md-3 left-col">
-                <div className="div">
-                <Link to=""><img className=' d-block' src={productDesc1} alt="" /></Link>
-                <Link to=""><img className=' d-block' src={productDesc2} alt="" /></Link>
+    return (
+        <div className="product-description-wrapper">
+            <div className="row p-2">
+                <div className="col-md-3 left-col">
+                    <div className="div">
+                        <Link to=""><img className=' d-block' src={productDesc1} alt="" /></Link>
+                        <Link to=""><img className=' d-block' src={productDesc2} alt="" /></Link>
+                    </div>
+                </div>
+                <div className="col-md-4 align-items-start mid-col">
+                    <Link to=""><img className='my-4 d-block' src={productDesc1} alt="" /></Link>
+                    <Link to=""><img className='my-4 d-block' src={productDesc2} alt="" /></Link>
+                </div>
+                <div className="col-md-5 right-col align-items-start">
+                    <h5 className="card-title text-start">44 CALIBER RING</h5>
+                    <div className='mt-1 about'>
+                        <div className="icons">
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                        </div>
+                        <div className="text">
+                            <p className='review'>1 review</p>
+                        </div>
+                    </div>
+                    <p className="card-text text-start price mt-4">1,500.00pkr</p>
+                    <hr />
+                    <div className="ring-size">
+                    <p>Ring Size:</p>
+                    <div className="btn-group dropstart">
+                        <button type="button" className="btn  dropdown-toggle w-100 p-0" data-bs-toggle="dropdown" aria-expanded="false">
+                            <h5 className='p-2'>16/ US 6</h5>
+                        </button>
+                        <ul className="dropdown-menu">
+                            <li><a href=""> <h5 className='p-1 sizes m-2'>16/ US 6</h5></a></li>
+                            <li><a href=""> <h5 className='p-1 sizes m-2'>16/ US 6</h5></a></li>
+                            <li><a href=""> <h5 className='p-1 sizes m-2'>16/ US 6</h5></a></li>
+                            <li><a href=""> <h5 className='p-1 sizes m-2'>16/ US 6</h5></a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div className="ring-color mt-3">
+                    <p>Color: {"Silver"}</p>
+                    <div className="color-btn">
+                        <button className='bg-warning p-3 me-3'></button>
+                        <button className='bg-secondary p-3 me-3'></button>
+                    </div>
+                    
+                    </div>
                 </div>
             </div>
-            <div className="col-md-4 align-items-start mid-col">
-                <Link to=""><img className='my-4 d-block' src={productDesc1} alt="" /></Link>
-                <Link to=""><img className='my-4 d-block' src={productDesc2} alt="" /></Link>
-            </div>
-            <div className="col-md-5 align-items-start">
-                <Link to=""><img className='w-25 d-block my-3' src={productDesc1} alt="" /></Link>
-                <Link to=""><img className='w-25 d-block my-3' src={productDesc2} alt="" /></Link>
-            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDescription
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './ProductCardStyles.css'
 import cardimg from '../../imgs/card1.png'
 import { Link } from 'react-router-dom'
+import reviewStar from '../../imgs/reviewStar.png'
 
 function ProductCard() {
 
-    
+
     return (
         <div className="product-card-wrapper">
             <div className="card border-0">
@@ -14,11 +15,11 @@ function ProductCard() {
                     <h5 className="card-title text-center">44 CALIBER RING</h5>
                     <div className='mt-2 about'>
                         <div className="icons mx-2">
-                            <span className="material-symbols-outlined">star</span>
-                            <span className="material-symbols-outlined">star</span>
-                            <span className="material-symbols-outlined">star</span>
-                            <span className="material-symbols-outlined">star</span>
-                            <span className="material-symbols-outlined">star</span>
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
+                            <img className="star" src={reviewStar} alt="" />
                         </div>
                         <div className="text">
                             <p className='review'>1 review</p>
@@ -26,7 +27,7 @@ function ProductCard() {
                     </div>
                     <p className="card-text text-center price">1,500.00pkr</p>
                     <div className="colors mt-3">
-                        <Link to="" className="btn btn-primary m-1 p-2"></Link> <Link to="#" className=" m-1 btn btn-warning  p-2"></Link> <Link to="#" className=" m-1 btn btn-dark  p-2"></Link> 
+                        <Link to="" className="btn btn-primary m-1 p-2"></Link> <Link to="#" className=" m-1 btn btn-warning  p-2"></Link> <Link to="#" className=" m-1 btn btn-dark  p-2"></Link>
                     </div>
                 </div>
             </div>
